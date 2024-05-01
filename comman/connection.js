273803aeb683d0f5702fb969752c9b2a9bfa854c
viejov3 <yugandharselvam@gmi=ail.com>
2024-05-02 01:56:04 +0530

    last change

--- comman/connection.js
+++ comman/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
-const Mongo_Url = process.env.ATLAS_SECRECT
+const Mongo_Url = "mongodb+srv://yugandharselvam:oBMopBiJiP6aFUBu@seaindia.rgzhgce.mongodb.net/"
 const Connect = () =>{ 
 mongoose.connect(Mongo_Url)
 .then(() => {console.log("Mongo DB is connected....");
